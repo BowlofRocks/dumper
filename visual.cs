3063808d68a178a4b8b00840b1ea2805cf57756b
choatecory <corychoate1999@gmail.com>
2022-10-18 20:12:41 -0600

    fixed visual class

---
+++ visual.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace dumper;
+
+public class visual
+{
+    public void parachute()
+    {
+        Console.WriteLine(" ___ ");
+        Console.WriteLine("/___\\");
+        Console.WriteLine("\\   /");
+        Console.WriteLine(" \\ /");
+        Console.WriteLine("  0");
+        Console.WriteLine(" /|\\");
+        Console.WriteLine(" / \\");
+    }
+}
