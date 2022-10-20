7b76d2ccc918e75b0bdfb7654af8096b3ecc43be
Paul Amago <apaulo2035@gmail.com>
2022-10-20 15:09:36 -0600

    words

---
+++ words.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace words{
+
+    
+    public class word_generator
+    {
+            private string[] wordList;
+            
+            public word_generator(){
+
+               
+            } 
+      
+            
+            private string Generate_word(){
+
+             }
+        
+        static readonly string textFile = @ "C:/dumper/wordlewords.txt"
+        string[] lines = File.ReadAllText(textFile);
+    }
+}
+
+
+
+// Reading a file
+//https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
